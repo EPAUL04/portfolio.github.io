8c20c1712de2f733c89c7562510f2e9b4231c175
Elizabeth Paul <eandepaul@gmail.com>
2026-07-27 16:19:01 -0700

    boxes won't scroll until clicked now yay

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 function dropdown() {
     document.getElementById("ai").classList.toggle("show");
 }
+
+function makeScrollable(name) {
+    document.getElementById(name).classList.add("scrollable");
+    document.getElementById(name + ".more").style.display  = 'none';
+}
